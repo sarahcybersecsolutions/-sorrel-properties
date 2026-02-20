e8fadd4315197e0093c04c361172c117a5dffd05
Sorrel Properties <sarah@sorrelproperties.com>
2026-02-20 15:38:36 +0300

    Add admin link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -52,6 +52,7 @@ const Footer = () => {
                 <li><Link to="/properties?type=apartment">Apartments</Link></li>
                 <li><Link to="/properties?type=condo">Condos</Link></li>
                 <li><Link to="/properties?type=commercial">Commercial</Link></li>
+                <li><Link to="#/admin" style={{fontSize: '0px', opacity: 0}}>Admin</Link></li>
               </ul>
             </div>
 
