bda481d9bea0fe79e944f306c70926d572dd80be
Sorrel Properties <sarah@sorrelproperties.com>
2026-02-20 07:51:54 +0300

    Fix deployment URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sorrel-properties/',
+  base: '/-sorrel-properties/',
   server: {
     port: 3000,
     open: true
